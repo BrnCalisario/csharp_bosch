abb77e5d921d1128329ebc8defbac6d7a53701c5
BrnCalisario <bruno145224@gmail.com>
2022-10-26 17:10:25 -0300

    alterações

--- aula_10/Program.cs
+++ aula_10/Program.cs
@@ -3,32 +3,47 @@
 public class Program
 {
 
-    
+
     static void Main(string[] args)
-    {   
-        while(true)
+    {
+        while (true)
         {
             Game g = new Game();
-            while(true)
+
+            bool shopState = false;
+
+
+            while (true)
             {
                 Console.Clear();
-                g.DrawHUD();
-                if(Console.ReadKey().Key == ConsoleKey.D0)
-                    g.Salgados++;
-                
+                if (!shopState)
+                {
+                    g.DrawMain();
+                    if (Console.ReadKey().Key == ConsoleKey.D0)
+                        g.Salgados++;
+                    else if (Console.ReadKey().Key == ConsoleKey.D1)
+                        shopState = true;
+                } 
+                else
+                {
+                    g.DrawShop();
+                }
+
+
             }
         }
     }
 
-    
+
 }
 
 
 public class Game
 {
     public int Salgados { get; set; }
+    Machine Rolo = new Rolo();
 
-    public void DrawHUD()
+    public void DrawMain()
     {
         string title = Visual.Label("Padoca Clicker");
         string money = Visual.Label($"Salgados: {this.Salgados}");
@@ -39,6 +54,11 @@ public class Game
         string pack = Visual.sideBySide(click, shop);
         Console.WriteLine(packTitle);
         Console.WriteLine(pack);
+    }
+
+    public void DrawShop()
+    {
+        Console.WriteLine(Visual.DoubleLabel(new string[] {Rolo.Name, Rolo.Description}));
     }
 }
 
@@ -59,6 +79,29 @@ public class Visual
         return top + "\n" + label + "\n" + bottom;
     }
 
+    public static string DoubleLabel(string[] labelTexts)
+    {
+
+        int maiorLength = 0;
+        for(int i = 0; i < labelTexts.Length; i++)
+        {
+            if (labelTexts[0].Length > maiorLength)
+            {
+                maiorLength = labelTexts[0].Length;
+            }
+        }
+
+        string pipe = "";
+        while (pipe.Length < (maiorLength - 2))
+        {
+            pipe += "═";
+        }
+        string top = "╔" + pipe + "╗";
+        string bottom = "╚" + pipe + "╝";
+
+        return "";
+    }
+
     public static string sideBySide(string label1, string label2)
     {
         string subL1 = label1.Substring(0, label1.IndexOf("\n") + 1);
@@ -77,7 +120,7 @@ public class Visual
 
         string line3 = label1 + label2;
 
-        return line1 + "\n" + line2 + "\n" + line3; 
+        return line1 + "\n" + line2 + "\n" + line3;
     }
 
     public static void Print(object obj)
