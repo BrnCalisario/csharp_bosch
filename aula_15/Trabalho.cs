6e1509e77c34af339fecbe43b8c6f66885c48a33
BrnCalisario <bruno145224@gmail.com>
2022-11-08 11:25:15 -0300

    Continuaçao csv das vacinas

--- aula_15/Trabalho.cs
+++ aula_15/Trabalho.cs
@@ -1,19 +1,53 @@
-﻿string path = "data/INFLUD21-24-10-2022.csv";
+﻿// string pathC = "data/INFLUD21-24-10-2022.csv";
+
+// var cabecalho = pathC.Open().First().Replace("\"", "").Split(';');
+// var indexVac = cabecalho.ToList().IndexOf("VACINA_COV");
+// Console.WriteLine(indexVac);
+
+// var table = pathC.Open().Read();
 
 
-var table = path.Open().Read();
-var mortos = table.Where(p => p.EvolucaoCaso == "Óbito").Count();
+// foreach(var i in table)
+// {
+//     Console.WriteLine(i.EvolucaoCaso);
+// }
 
+var path = "COVID_21.csv";
 
-
-Console.WriteLine(mortos);
+var table = path.Open().ReadCovid();
+Console.WriteLine(table.Count());
 
 public delegate void Print(object x);
 
+
+public enum TipoDose
+{
+    Indefinido = -1,
+    PrimeiraDose = 0,
+    SegundaDose = 1,
+    DoseReforco = 2
+}
+
 public class CasoCovid 
 {
+
+
+
     public string EvolucaoCaso { get; set; }
     public string DataFinal { get; set; }
+    public List<Vacina> Vacinas { get; set; }
+
+    public void addVacina(string data=null, string lote=null, string fabricante=null, int tipoDose = -1)
+    {
+        var vacina = new Vacina();
+
+        vacina.Data = (data != null ? data : "N/A");
+        vacina.Lote = (lote != null ? lote : "N/A");
+        vacina.Fabricante = (fabricante != null ? fabricante : "N/A");
+        vacina.TipoDose = ( tipoDose > -1 ? (TipoDose) tipoDose : TipoDose.Indefinido) ;
+
+        Vacinas.Add(vacina);
+    }
 
     public CasoCovid(string evolucao)
     {
@@ -37,6 +71,15 @@ public class CasoCovid
         }
     }
 }
+
+public struct Vacina
+{
+    public TipoDose TipoDose { get; set; }
+    public string Data { get; set; }
+    public string Lote { get; set; }
+    public string Fabricante { get; set; }
+}
+
 
 public static class ExtensionMethods
 {
@@ -81,6 +124,14 @@ public static class ExtensionMethods
         throw new Exception();
     }
 
+    public static void WriteCSV(this string[] coll, string path)
+    {
+        var stream = new StreamWriter(path, true);
+    
+        stream.WriteLine(string.Join(';', coll));
+
+        stream.Close();
+    }
 
     public static IEnumerable<CasoCovid> Read(this IEnumerable<string> csv)
     {
@@ -88,8 +139,10 @@ public static class ExtensionMethods
         it.MoveNext();
         var header = it.Current.Replace("\"", "").Split(';');
 
-        var classIndex = header.ToList().IndexOf("CLASSI_OUT");
+        var classIndex = header.ToList().IndexOf("CLASSI_FIN");
         var evoIndex = header.ToList().IndexOf("EVOLUCAO");
+
+        header.WriteCSV("COVID_21.csv");
 
         while(it.MoveNext())
         {
@@ -97,8 +150,46 @@ public static class ExtensionMethods
 
             if(item.ElementAt(classIndex + 1) == "5")
             {
+                item.WriteCSV("COVID_21.csv");
                 yield return new CasoCovid(item.ElementAt(evoIndex + 1));
             } 
+        }
+    }
+
+    public static IEnumerable<CasoCovid> ReadCovid(this IEnumerable<string> csv)
+    {
+        var it = csv.GetEnumerator();
+        it.MoveNext();
+        var header = it.Current.Replace("\"", "").Split(';').ToList();
+
+        var evoIndex = header.IndexOf("EVOLUCAO") + 1;
+        var covIndex = header.IndexOf("VACINA_COV") + 1;
+
+        while(it.MoveNext())
+        {
+            var item = it.Current.Split(";");
+
+            CasoCovid caso = new CasoCovid(item.ElementAt(evoIndex));
+
+
+            if (item.ElementAt(covIndex) == "1")
+            {
+                string[] vals = new string[] { "1_COV", "2_COV", "REF"};
+                string[] fabs  = new string[] { "COV_1", "COV_2", "COVREF"};
+                for(int i = 0; i < 3; i++)
+                {  
+                    var data = item.ElementAt(header.IndexOf($"DOSE_{vals[i]}") + 1);
+                    var lote = item.ElementAt(header.IndexOf($"LOTE_{vals[i]}") + 1);
+                    var fab = item.ElementAt(header.IndexOf($"FAB_{fabs[i]}") + 1);
+
+                    caso.addVacina(data, lote, fab, i);
+                }   
+                
+              
+            }
+
+            yield return caso;
+
         }
     }
 }
